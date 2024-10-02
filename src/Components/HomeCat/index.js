114c1981f4c2f9b24944438e1685e49b739ac1ce
quynhhla2 <118544865+quynhhla2@users.noreply.github.com>
2024-10-02 13:17:47 +0700

    Product Item Added

---
+++ src/Components/HomeCat/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+
+const HomeCat = () => {
+  const [itemBg, setItemBg] = useState([
+    "#fffceb",
+    "#ecffec",
+    "#feefea",
+    "#fff3eb",
+    "#fff3ff",
+    "#f2fce4",
+    "#feefea",
+    "#fffceb",
+    "#feefea",
+    "#ecffec",
+    "#feefea",
+    "#fff3eb",
+    "#fff3ff",
+    "#f2fce4",
+    "#feefea",
+    "#fffceb",
+    "#feefea",
+    "#ecffec",
+  ]);
+
+  return (
+    <section className="homeCat">
+      <div className="container">
+        <h3 className="mb-3 hd">Featured Categories</h3>
+        <Swiper
+          slidesPerView={10}
+          spaceBetween={8}
+          navigation={true}
+          slidesPerGroup={5}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          {itemBg?.map((item, index) => {
+            return (
+              <SwiperSlide>
+                <div className="item text-center cursor" style={{ background: item }}>
+                  <img
+                    src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png"
+                    alt="..."
+                  />
+
+                  <h6>Red Apple</h6>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default HomeCat;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,20 +2,28 @@ import HomeBanner from "../../Components/HomeBanner";
 import Button from "@mui/material/Button";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import React from "react";
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import ProductItem from "../../Components/ProductItem";
+import HomeCat from "../../Components/HomeCat";
+import Poster1 from '../../assets/images/poster1.jpeg';
+import Poster2 from '../../assets/images/poster2.jpeg';
 
 const Home = () => {
-  var productSliderOptions = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1
-  };
+  // var productSliderOptions = {
+  //   dots: true,
+  //   infinite: false,
+  //   speed: 500,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  // };
 
   return (
     <>
       <HomeBanner />
+      <HomeCat />
 
       <section className="homeProducts">
         <div className="container">
@@ -23,7 +31,14 @@ const Home = () => {
             <div className="col-md-3">
               <div className="banner">
                 <img
-                  src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/banner-box.jpg"
+                  src={Poster1}
+                  className="cursor w-100"
+                  alt="..."
+                />
+              </div>
+              <div className="banner mt-4">
+                <img
+                  src={Poster2}
                   className="cursor w-100"
                   alt="..."
                 />
@@ -44,18 +59,88 @@ const Home = () => {
                 </Button>
               </div>
 
-              <div className="product_row">
-                <Slider {...productSliderOptions}>
-                  <div className="item productItem">
-                    <div className="imgWrapper">
-                      <img
-                        src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62-346x310.jpg"
-                        className="w-100"
-                        alt="..."
-                      />
-                    </div>
-                  </div>
-                </Slider>
+              <div className="product_row w-100 mt-4">
+                <Swiper
+                  slidesPerView={4}
+                  spaceBetween={0}
+                  navigation={true}
+                  slidesPerGroup={3}
+                  modules={[Navigation]}
+                  className="mySwiper"
+                >
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+
+              <div className="d-flex align-items-center mt-5">
+                <div className="info w-75">
+                  <h3 className="mb-0 hd">NEW PRODUCTS</h3>
+                  <p className="text-light text-sml mb-0">
+                    New products with updated stocks.
+                  </p>
+                </div>
+
+                <Button className="viewAllBtn ml-auto">
+                  View All <IoIosArrowRoundForward />
+                </Button>
+              </div>
+
+              <div className="product_row w-100 mt-4">
+                <Swiper
+                  slidesPerView={4}
+                  spaceBetween={0}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  modules={[Navigation]}
+                  className="mySwiper"
+                >
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductItem />
+                  </SwiperSlide>
+                </Swiper>
               </div>
             </div>
           </div>
